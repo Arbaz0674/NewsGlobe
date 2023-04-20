385b1c4311388a91d6262ac5c3693d3fce644391
ARBAZ KHAN <khanarbaz0674@gmail.com>
2023-04-21 00:44:05 +0530

    Introduced Pagination in an Application

--- src/components/News.js
+++ src/components/News.js
@@ -6,20 +6,49 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
+      page: 1,
     };
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=863141b831d34fa5aa81fdf00014fc93`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=863141b831d34fa5aa81fdf00014fc93&page=${this.state.page}&pageSize=20`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
+
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=863141b831d34fa5aa81fdf00014fc93&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles, page: this.state.page - 1 });
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=863141b831d34fa5aa81fdf00014fc93&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1,
+      });
+    }
+  };
 
   render() {
     return (
       <div className="container my-3">
-        <h1>Today's Top Headlines</h1>
+        <h1 style={{ marginTop: "4rem" }}>Today's Top Headlines</h1>
         <div className="row">
           {this.state.articles.map((element) => {
             return (
@@ -35,6 +64,23 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePreviousClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,15 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl? imageUrl : `https://thumbs.dreamstime.com/z/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg`} className="card-img-top" alt="..." />
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : `https://thumbs.dreamstime.com/z/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg`
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">
               {title.length < 83 ? title : `${title}...`}
@@ -15,7 +23,8 @@ export class NewsItem extends Component {
             <a
               href={newsUrl}
               target="_blank"
-              className="btn btn-sm btn-primary"
+              className="btn btn-sm btn-primary btn-dark"
+              rel="noreferrer"
             >
               Read More
             </a>
